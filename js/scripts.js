ce172df2a733cd517bb2fbd731b11f363617472a
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-28 18:09:53 +0200

    Add sample joke to loaded website

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 var url = 'http://api.icndb.com/jokes/random';
 var button = document.getElementById('get-joke');
+getJoke()
 button.addEventListener('click', function() {
+	getJoke();
+});
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', url);
 	xhr.addEventListener('load', function() {
@@ -8,5 +14,5 @@ button.addEventListener('click', function() {
 		paragraph.innerHTML = response.value.joke;
 	});
 	xhr.send();
-});
+}
 var paragraph = document.getElementById('joke');
